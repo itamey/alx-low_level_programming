3fb93cd013475d9f65b1072d1d0bb0baaa9cc0df
Shalom <eyoshalom@yahoo.com>
2022-09-08 15:06:22 -0700

    print a message

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+/**
+ * main entry point for the program
+ *
+ * Return : Display a message to the terminal
+ */
+
+ints main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0)
+}
