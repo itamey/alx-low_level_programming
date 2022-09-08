4e7d953228c5aaacaaab9853de68af20deabad23
Shalom <eyoshalom@yahoo.com>
2022-09-08 15:20:39 -0700

    use puts to print a name

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 
-
 /**
- * main entry point for the program
+ * main - entry point for the program
  *
- * Return : Display a message to the terminal
+ * Return: Display a message to the terminal
  */
 
-ints main(void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0)
+	return(0);
 }
