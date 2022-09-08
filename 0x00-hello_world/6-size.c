5ea5436a342efc8d35b500d115ce2dd4659408ad
Shalom <eyoshalom@yahoo.com>
2022-09-08 16:36:08 -0700

    data type sizes using suzeof

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,12 +9,10 @@
 
 int main(void)
 {
-	printf("Size of char is %d byte(s)\n", sizeof(char));
-	printf("Size of int is %d byte(s)\n", sizeof(int));
-
-	printf("Size of float is %d byte(s)\n", sizeof(float));
-	printf("Size of long int is %d byte(s)\n", sizeof(long));
-	printf("Size of long long int is %d byte(s)\n", sizeof(long long));
-	printf("Size of unsigned int is %d byte(s)\n", sizeof(unsigned int));
+	printf("Size of char: %d byte(s)\n", sizeof(char));
+	printf("Size of int: %d byte(s)\n", sizeof(int));	
+	printf("Size of long int: %d byte(s)\n", sizeof(long));
+	printf("Size of long long int: %d byte(s)\n", sizeof(long long));
+	printf("Size of float: %d byte(s)\n", sizeof(float));
 	return (0);
 }
