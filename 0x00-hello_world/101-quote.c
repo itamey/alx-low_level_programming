dac1bb432b579e31604d66e2dbdfca340aad2c7d
Shalom <eyoshalom@yahoo.com>
2022-09-09 15:12:06 -0700

    quotes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,12 +4,12 @@
 /**
  * main - Prints out the last part of a quote in stabdard error
  * Description: write a piece of code
- * return: 1 (success)
+ * return: Zero as Success
  */
 
 int main(void)
 {
 	write(2,
-	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
