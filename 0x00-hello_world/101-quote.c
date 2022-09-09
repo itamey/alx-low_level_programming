df44fec22f178839ac6422feb09617b52268c35f
Shalom <eyoshalom@yahoo.com>
2022-09-09 02:55:25 -0700

    quotes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <string.h>
-#include <unistd.h>
+
 /**
  * main - main block
+ * Description: write a piece of code
  * return: 1
  */
 int main(void)
 {
-	int s = strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n,s");
+	int s = strlen("and that piece of art is useful\"-Dora Korpar, 2015\-10\-19\n");
+	write(2,
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
