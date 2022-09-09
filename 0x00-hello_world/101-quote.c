986e12babb1d84bc818b59aa85f1807ff3631959
Shalom <eyoshalom@yahoo.com>
2022-09-09 14:47:01 -0700

    quotes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 #include <string.h>
+#include <stderr.h>
 
 /**
- * main - main block
+ * main - passes an arguement
  * Description: write a piece of code
- * return: 1
+ * return: 1 (success)
  */
+
 int main(void)
 {
-	int s = strlen("and that piece of art is useful\"-Dora Korpar, 2015\-10\-19\n");
+	strlen("and that piece of art is useful\"-Dora Korpar, 2015-10-19\n");
+
 	write(2,
 	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
