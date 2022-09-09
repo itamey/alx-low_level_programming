00e3693226a56b38f3a6a97f68c0df1c209cd837
Shalom <eyoshalom@yahoo.com>
2022-09-09 15:03:45 -0700

    quotes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
-#include <string.h>
+#include <unistd.h>
 
 /**
- * main - passes an arguement
+ * main - Prints out the last part of a quote in stabdard error
  * Description: write a piece of code
  * return: 1 (success)
  */
 
 int main(void)
 {
-	strlen("and that piece of art is useful\"-Dora Korpar, 2015-10-19\n");
-
 	write(2,
 	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
