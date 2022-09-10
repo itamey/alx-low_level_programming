5157fc74156d8bad64e7aa1fa600a70ad96870c3
Shalom <eyoshalom@yahoo.com>
2022-09-10 12:10:19 -0700

    numbers of bas4 ten

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry of program
+ * Description:prints number of base 10 starting from 0 
+ * Return: Always (success)
+ */
+
+int main(void)
+{
+	int c;
+
+	for (c = 0; c <= 9; c++)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
