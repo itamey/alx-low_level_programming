ea077a9991da17a16c549326a71c500243f263fe
Shalom <eyoshalom@yahoo.com>
2022-09-10 04:36:11 -0700

    prunt alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/** main - entry of program
+ *
+ * Return: Always (success)
+ */
+
+int main(void)
+{
+	char c;
+
+	for(c = 'a'; c <= 'z';c++)
+		putchar(c);
+	putchar(`\n');
+	return (0);
+}
