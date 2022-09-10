37dfb447d2e675096fecb4f1903afa97c4862ca1
Shalom <eyoshalom@yahoo.com>
2022-09-10 13:18:01 -0700

    reverse alpha

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry of program
+ * Description:prints all alphabetsn reverse order
+ * Return: Always (success)
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c => 'a'; c--;)
+		putchar(c);
+
+	putchar('\n');
+
+	return (0);
+}
