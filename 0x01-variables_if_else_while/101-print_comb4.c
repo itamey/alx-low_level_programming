cd1c753565e77ff16a538ed9ea5f14376088906c
Shalom <eyoshalom@yahoo.com>
2022-09-11 11:06:09 -0700

    print triple

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -16,13 +16,13 @@ int main(void)
 	{
 		for (b = (a + 1); b <= 9; b++)
 		{
-			for (c = (b + 1); c <= 9; c++)
+			for (c = (b + 1); c <= 8; c++)
 			{
 				putchar('0' + a);
 				putchar('0' + b);
 				putchar('0' + c);
 
-				if (a < 8)
+				if (a < 7)
 				{
 				putchar(',');
 				putchar(' ');
