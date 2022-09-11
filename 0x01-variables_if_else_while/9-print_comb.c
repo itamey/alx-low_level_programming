3cb77534767df755e316c9fbca50dda4f0791641
Shalom <eyoshalom@yahoo.com>
2022-09-10 21:32:05 -0700

    with comma

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - entry of program
+ * Description:prints base sixteen in lower case
+ * Return: Always (success)
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+		putchar(c);
+		if (9 != 0)
+		putchar(',');
+		putchar('');
+	putchar('\n');
+
+	return (0);
+}
