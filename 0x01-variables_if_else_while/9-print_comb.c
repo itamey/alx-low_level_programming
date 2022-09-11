d3caee39afa4930f6ba0fbadd8d1f054fed5b9a1
Shalom <eyoshalom@yahoo.com>
2022-09-10 21:58:43 -0700

    print with commas

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -2,7 +2,7 @@
 
 /**
  * main - entry of program
- * Description:prints base sixteen in lower case
+ * Description: prints base sixteen in lower case
  * Return: Always (success)
  */
 
@@ -12,7 +12,7 @@ int main(void)
 
 	for (c = '0'; c <= '9'; c++)
 		putchar(c);
-		if (9 != 0)
+		if (c != 9)
 		putchar(',');
 		putchar('');
 	putchar('\n');
