52eeb9952dc4994bd529252a4a847081c0d0140e
Shalom <eyoshalom@yahoo.com>
2022-09-13 08:48:25 -0700

    shows error dor lower case c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _islower - entry of program
+ * Description:   print alphabet except lower case c
+ * Return: returns one if c is present else
+ * returns zero
+ */
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	}
+		return (0);
+	}
+}
