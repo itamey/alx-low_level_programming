fc1526e792ee10ffb975982a56881991bc8aa846
Shalom <eyoshalom@yahoo.com>
2022-09-13 07:40:33 -0700

    print alphabets10 ten

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * print_alphabet - entry of program
+ * Description:  code for print alphabets
+ * Return: no return value
+ */
+
+void print_alphabet_x10(void)
+{
+	char a = 'a';
+	int b = 10;
+
+	while (b < 10)
+	}
+		while (a <= 'z')
+		{
+			putchar(a);
+			a++;
+		}
+		putchar('\n');
+		b++
+	}
+}
