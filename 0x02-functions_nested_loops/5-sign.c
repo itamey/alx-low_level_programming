000afb924a415aeb3100a99aaf4a3494aa9f450b
Shalom <eyoshalom@yahoo.com>
2022-09-13 12:17:08 -0700

    print signs

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_sign - entry of program
+ * Description:   print sign of values assigned
+ * @c: the character code to evaluate
+ * Return: returns - if it is below 
+ * 0 and 1 if it above zero
+ * returns zero
+ */
+
+int print_sign(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		return (1);
+	}
+	else if (n > 1)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
+
