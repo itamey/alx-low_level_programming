256992a9c27ddfd266bba308dff5346791533b27
Shalom <eyoshalom@yahoo.com>
2022-09-13 20:31:48 -0700

    print 9x time

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - entry of program
+ * Description:  print the sum of two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: returns a+b
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
