a76cc19595199ae11f3d999debbdaae158dd447c
Shalom <eyoshalom@yahoo.com>
2022-09-14 01:05:22 -0700

    debugging comment

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * main - entry of program
+ * Description: causes an infinite loop
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	printf("Infinte loop incoming : (\n");
+	i = 0;
+
+	/**
+	 * while (i < 10)
+	{
+		putchar(i);
+	}
+	*/
+	printf("Infinte loop avoided\\o/\n");
+
+	return (0);
+}
