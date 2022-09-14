7548c382f80c3a57fb1fa62a75edf185f237b496
Shalom <eyoshalom@yahoo.com>
2022-09-13 20:20:42 -0700

    print 9x time

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * times_table- entry of program
+ * Description: prints the 9x9 table
+ * Return: void 
+ */
+
+void times_table(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			int temp = i * j;
+
+			if (temp / 10)
+			{
+				int a;
+				int b;
+
+				a = temp / 10;
+				b = temp % 10;
+				_putchar('0' + a);
+				_putchar('0' + b);
+			}
+			else
+			{
+				if (j > 0)
+				{
+					_putchar(' ');
+				}
+				_putchar('0' + temp);
+			}
+
+			if (j < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n;);
+	}
+}
