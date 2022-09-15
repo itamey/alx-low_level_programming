a8ab4671ef5abc7e33b751b9f5da26a13d15e000
Shalom <eyoshalom@yahoo.com>
2022-09-15 09:20:36 -0700

    print sqaure

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_square - entry of program
+ * @size: this represents the size of the square
+ * Description: program to form square using # on the terminal
+ * Return: void
+ */
+
+void print_square(int size)
+{
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i, j;
+
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#');
+			}
+			
+			_putchar('\n');
+		}
+	}
+}
