d31043e0a7ea3f69b220031661f79aa4d4c9f806
Shalom <eyoshalom@yahoo.com>
2022-09-19 11:58:53 -0700

    pointers task0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - entry of program
+ * @n: pointer to a variable
+ *
+ * Description: This program takes a pointer to an int as parameter and updates the value it points to 98
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
