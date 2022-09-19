4fa02ed5ad8d266ad30618419f678416d872f88e
Shalom <eyoshalom@yahoo.com>
2022-09-19 13:30:29 -0700

    task6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character in a string
+ * @str: string to be printed out to the screen
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	int count = 1;
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (count % 2 != 0)
+		{
+			_putchar(str[i]);
+		}
+		++count;
+		i+++;
+	}
+	_putchar('\n');
+}
