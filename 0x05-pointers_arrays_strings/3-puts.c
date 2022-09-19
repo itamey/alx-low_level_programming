328eb0145779f2ab41ed17a4b9aa57be93a83923
Shalom <eyoshalom@yahoo.com>
2022-09-19 12:34:22 -0700

    pointers task3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _puts - entry of program
+ * @str: string to be printed out to the screen
+ * Description: This program prints a string followed by a new line
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
