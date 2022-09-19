3c5cdfc238f6c1484655df6d9f64c03cebf84da0
Shalom <eyoshalom@yahoo.com>
2022-09-19 12:58:23 -0700

    pointers task3

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include "2-strlen.c"
+
+/**
+ * print_rev - functions that prints a string in reverse folowed by a new line
+ * @s: the pointer to the string variable
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int length;
+	int i;
+
+	length = _strlen(s);
+	for (i = length - 1; i >= 0; i--)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
