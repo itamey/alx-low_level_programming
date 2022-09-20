811becfdac074dbf43cf0384ba0422e1e77317d9
Shalom <eyoshalom@yahoo.com>
2022-09-20 09:04:24 -0700

    task

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * _atoi - converts string to an integer
+ * @s: string to be converted to integer
+ * Return: Resulting integer number
+ */
+
+int _atoi(char *s)
+{
+	int i;
+	int sign;
+	unsigned int num;
+	
+	i = 0;
+	sign = 1;
+	num = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] = '-')
+		{
+			sign = -sign;
+		}
+		for (; s[i] >= '0' && s[i] <= ''; i++)
+		{
+			num = num * 10 + ((int)s[i] - '0');
+		}
+		if (num > 0 || s[i] == '\0')
+		{
+			break;
+		}
+		i++;
+	}
+	
+	return (num * sign);
+}
