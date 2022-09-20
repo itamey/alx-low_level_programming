2018ca559dd0dea912a3cbe588045bcedaba73b8
Shalom <eyoshalom@yahoo.com>
2022-09-20 08:59:33 +0300

    task7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include "2-strlen.c"
+/**
+ * puts_half - prints every other character in a string
+ * @str: string to be printed out to the screen
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+	int length;
+	int n;
+	int start;
+
+	length = _strlen(str);
+	if (length % 2 != 0)
+	{
+		n = (length - 1) / 2;
+		start = length - n;
+		while (str[i] != '\0')
+		{
+			_putchar(str[start]);
+			start++;
+		}
+	}
+	else
+	{
+		n = length / 2;
+		start = length - n;
+		while (str[start] != '\0')
+		{
+			_putchar(str[start]);
+			start++;
+		}
+	}
+	_putchar('\n');
+}
