5a5329bd8522471074e1f8a074139adde4d8a1e3
Shalom <eyoshalom@yahoo.com>
2022-09-27 05:24:01 -0700

    return a character if foynd

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character in a string
+ * @s: string to search within
+ * @c: character to searvh for within s
+ * Return: @s or null depending on what character is found
+ */
+
+char *_strchr(char *s, char c, unsigned int n)
+{
+	while (*s != '0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+
+	return (0);
+}
