3e4844f02d936de1fdf53ea3ca5d2428fcaa84df
Shalom <eyoshalom@yahoo.com>
2022-09-29 08:03:43 -0700

    check palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -39,5 +39,22 @@ int palindrome_checker(char *s, int beg, int end)
 	{
 		return (palindrome_checker9(s, beg + 1, end - 1));
 	}
+}
+/**
+ * _strlen_recursion - returns the length of the string
+ * @s: string to examine
+ * Return: length of @s
+ */
 
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	else
+	{
+		s++;
+		return (1 + _strlen_recursion(s));
+	}
 }
