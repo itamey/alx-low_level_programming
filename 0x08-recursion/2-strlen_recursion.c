962c6012315a8ee499a682615bb4852b9d7f33d0
Shalom <eyoshalom@yahoo.com>
2022-09-29 02:48:57 -0700

    returns length of string

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns the length of the string
+ * @s: string to examine
+ * Return: length of @s
+ */
+
+void _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	else
+	{
+		s++;
+		return (1 + _strlen_recursion(s);
+	}
+}
