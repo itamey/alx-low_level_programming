32530a96b1a9740b549f4fb57f8e8953216d7f4a
Shalom <eyoshalom@yahoo.com>
2022-09-30 10:06:16 -0700

    prints cl arg to the screen

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints all commandline arguements to the screen
+ * @argc: count of all the commandline arguments
+ * @argv: vector containing all the commandline arguments
+ * Return: Always 0 (Successful)
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argc[i]);
+
+	return (0);
+}
