9a6aa766133246f99d727230358b06d781e163ca
Shalom <eyoshalom@yahoo.com>
2022-09-30 10:21:37 -0700

    prints product of cl arguments

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - calculate and prints the product of commandline arguments
+ * @argc: count of all the commandline arguments
+ * @argv: vector containing all the commandline arguments
+ * Return: Always 0 (Successful)
+ */
+
+int main(int argc, char *argv[])
+{
+	int num1, num2;
+	
+	if (argc == 3)
+	{
+		num1 = atoi(argv[1]);
+		num2 = atoi(argv[2]);
+		printf("%d\n", (num1 * num2));
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	return (0);
+}
