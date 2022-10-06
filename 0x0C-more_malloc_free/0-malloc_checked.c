09f1d119a4bc562a34c224a7c0f6d7bab6500b77
Shalom <eyoshalom@yahoo.com>
2022-10-06 09:13:25 -0700

    mallock check

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * malloc_checked - uses malloc to assign dynamic memory
+ * @b: size of memory to allocate
+ * Return: void
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+	if (ptr == NULL)
+	{
+		exit(98);
+	}
+	return (ptr);
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,1 @@
+more malloc free exercises un c programming
