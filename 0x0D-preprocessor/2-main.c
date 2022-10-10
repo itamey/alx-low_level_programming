66a2975f83716699580f0eef740886eb64fa44d5
Shalom <eyoshalom@yahoo.com>
2022-10-10 08:49:58 -0700

    print filename to screen

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main -prints the name of current file to the screen
+ * follow by a new line
+ * Return : Always Success (0)
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
