b6da12755038f11d3625bd616652ab95124992dd
Shalom <eyoshalom@yahoo.com>
2022-10-10 19:47:13 -0700

    type struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,35 +1,35 @@
-#ifndef DOG_H_ 
-#define DOG_H_ 
-  
-/** 
- * struct dog - dog data type 
- * @name: Name of the dog 
- * @age: Age of the dog 
- * @owner: Dog's owner's name 
- * 
- * Description: Data type that can be used to represent a dog 
- * in out code. 
- */ 
-struct dog 
-{ 
-        char *name; 
-        float age; 
-        char *owner; 
-}; 
- 
-/** 
- * dog_t - Typedef of struct dog 
- * @name: Name of dog 
- * @age: Age of dog 
- * @owner: Owner of dog 
- * 
- * Description: alias for type struct dog 
- */ 
-typedef struct dog dog_t; 
+#ifndef DOG_H_
+#define DOG_H_
 
-/* Function Declarations */ 
-void init_dog(struct dog *d, char *name, float age, char *owner); 
-void print_dog(struct dog *d); 
-dog_t *new_dog(char *name, float age, char *owner); 
-void free_dog(dog_t *d); 
+/**
+ * struct dog - dog data type
+ * @name: Name of dog
+ * @age: age of dog
+ * @owner: name of dog's owner
+ * Description: data type used to identify dog in a code
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+/**
+ * dog_t - typedef of atruct dog
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner's name
+ * Description: alias for typedef atruct dog
+ */
+
+typedef struct dog dog_t;
+
+/* function prototypes */
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
 #endif
