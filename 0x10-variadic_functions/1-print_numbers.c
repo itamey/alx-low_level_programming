2260f406662b72c79cb124d2208198868751eb7e
Shalom <eyoshalom@yahoo.com>
2022-10-15 01:08:04 -0700

    print numbers

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,31 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_numbers - prints number seperated by a character to a screen
+ * @n: number of elements to print
+ * Return: @n
+ */
+
+void print_numbers(const char *seperator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list args;
+
+	va_start(args, n);
+	
+	for (i = 0; i < n; i++)
+	{
+		prinyf("%d", va_arg(args, int));
+		if (i < n - 1)
+		{
+			if (seperator)
+				printf("%s", seperator);
+		}
+	}
+
+	va_end(args);
+	return ("\n");
+}
